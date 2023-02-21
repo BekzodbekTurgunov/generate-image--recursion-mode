4cae2b0aa0862ec060878135af37faf339baf298
Bekzodbek Turgunov <bekzodbekt1999@gmail.com>
2023-02-22 00:40:52 +0500

    removed unused logs

--- app.js
+++ app.js
@@ -3,24 +3,16 @@ const fs = require("fs");
 const canvas = createCanvas(400, 400);
 const ctx = canvas.getContext('2d');
 
-
 async function createRecursiveImage(imageFilePath) {
     const image = await loadImage(imageFilePath);
-
-    const deltaX = 20; // The amount by which to move each level of recursion to the right
-
+    const deltaX = 20;
     function drawImageRecursively(level, x, y, size, deltaX) {
-        // Draw the image at the current position and size
         ctx.drawImage(image, x, y, size, size);
-
-        // Check if we should continue drawing recursively
         if (level > 1) {
-            // Calculate the size and position of the next image
             const nextSize = size / 2;
             const nextX = x + size / 4 + deltaX;
             const nextY = y + size / 4;
 
-            // Draw the next image recursively
             drawImageRecursively(level - 1, nextX, nextY, nextSize, deltaX + 10);
         }
     }
